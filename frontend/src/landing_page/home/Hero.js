8e35b52d5d8a69e8702dc4f371bb545f3ecac6a8
Delta Student <avishkashah3@gmail.com>
2026-08-07 20:25:33 +0530

    Updated basic home page layout

--- frontend/src/landing_page/home/Hero.js
+++ frontend/src/landing_page/home/Hero.js
@@ -1,8 +1,22 @@
 import React from 'react';
 function Hero() {
     return (
-        <h1>Hero<i class="fa fa-user-circle" aria-hidden="true"></i></h1>
-  );
+        <div className="container p-5 mb-5">
+            <div className="row text-center">
+                <div className="col">
+                    <img
+                        src="media/images/homeHero.png"
+                        alt="Hero"
+                        className="img-fluid mb-5"
+                        style={{ width: "80%", margin: "0 auto", display: "block" }}
+                    />                    
+                    <h1 className="mt-5">Invest in Everything </h1>
+                    <p>Online platform to invest in stocks, derivatives, mutual funds and more.</p>
+                    <button className="p-2 btn btn-primary fs-5 mb-5" style={{width:"20%", margin: "0 auto"}}>Signup Now</button>
+                </div>
+            </div>
+        </div>
+    );
 }
 
-export default Hero;
+export default Hero; 
